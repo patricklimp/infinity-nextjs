2883e679712255207ed686cc52a73aacd8d6549f
Patrick Limp <103951855+patricklimp@users.noreply.github.com>
2024-12-17 17:39:36 -0300

    atualização - novas paginas

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,10 @@
 export default function login(){
     return (
         <section className="login">
-            <div>
-                <img src="public/style/assets/spotify.png" alt="Logo do Spotify" />
-            <button className="login-spotify">Conecte-se com spotify</button>
+            <div className="login-spotify">
+                <img className="image-logo" src="style/assets/spotify.png" alt="Logo do Spotify" />
+                <button>Conecte-se com spotify</button>
             </div>
-            
-            <button className="login-apple">Conecte-se com Apple Music</button>
         </section>
     )
 }
