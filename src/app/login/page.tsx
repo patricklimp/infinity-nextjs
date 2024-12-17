bdb5ac1b32be3221893ca909040e6958cae747d1
Patrick Limp <103951855+patricklimp@users.noreply.github.com>
2024-12-17 13:08:37 -0300

    atualização

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+export default function login(){
+    return (
+        <section className="login">
+            <div>
+                <img src="public/style/assets/spotify.png" alt="Logo do Spotify" />
+            <button className="login-spotify">Conecte-se com spotify</button>
+            </div>
+            
+            <button className="login-apple">Conecte-se com Apple Music</button>
+        </section>
+    )
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function profile(){
+    return (
+        <div className=" text-white">
+            pagina do perfil
+        </div>
+    )
+}
